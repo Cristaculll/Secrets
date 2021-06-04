29a93fe41f0077d6b7e6f9669944cff1685489a1
Cristaculll <81773980+Cristaculll@users.noreply.github.com>
2021-06-04 00:18:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -173,11 +173,6 @@ req.login(user,function(err){
 
 });
 
-let port = process.env.PORT;
-if (port == null || port == ""){
-  port = 3000;
-}
-
-app.listen(port, function() {
+app.listen(process.env.PORT || 3000, function() {
  console.log("Server started successfully.");
 });
